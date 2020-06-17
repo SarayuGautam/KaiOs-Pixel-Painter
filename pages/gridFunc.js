bb6271ebb2b9658bcc3db246986f158a57f1397c
Sarayu Gautam <sarayugautam1@gmail.com>
2020-06-17 12:31:51 +0545

    implementing custom color picker

--- pages/gridFunc.js
+++ pages/gridFunc.js
@@ -9,7 +9,11 @@ $(document).ready(function () {
   const availableColors=[];
   const numberOfColors=availableColors.length;
   var isGrid =true;
-  
+  const paletteColors=["#003366","#336699","#3366CC","#003399","#000099","#0000CC","#000066","#006666","#006699","#0099CC","#0066CC","#0033CC","#0000FF","#3333FF","#333399","#669999","#009999","#33CCCC","#00CCFF","#0099FF","#0066FF","#3366FF","#3333CC","#666699","#339966","#00CC99","#00FFCC","#00FFFF","#33CCFF","#3399FF","#6699FF","#6666FF","#6600FF","#6600CC","#339933","#00CC66","#00FF99","#66FFFF","#99CCFF","#9999FF","#9966FF","#9933FF","#9900FF","#006600","#00CC00","#00FF00","#66FF99","#99FFCC","#CCCCFF","#CC99FF","#CC66FF","#CC33FF","#CC00FF","#9900CC","#003300","#009933","#33CC33","#66FF66","#99FF99","#CCFFCC","#FF99FF","#FF66FF","#FF00FF","#CC00CC","#660066","#336600","#009900","#66FF33","#99FF66","#CCFF99","#FFFFCC","#FFCCCC","#FF99CC","#FF33CC","#CC0099","#993399","#333300","#669900","#99FF33","#CCFF66","#FFFF99","#FFCC99","#FF9999","#FF6699","#FF3399","#CC3399","#990099","#666633","#99CC00","#CCFF33","#FFFF66","#FFCC66","#FF9966","#FF6666","#FF0066","#CC6699","#993366","#999966","#CCCC00","#FFFF00","#FFCC00","#FF9933","#FF6600","#FF5050","#CC0066","#660033","#996633","#CC9900","#FF9900","#CC6600","#FF3300","#FF0000","#CC0000","#990033","#663300","#996600","#CC3300","#993300","#990000","#800000","#993333"];
+ 
+  const numberOfPaletteColors= Math.floor(Math.sqrt(paletteColors.length));
+  const sizeOfColor=canvasSize/numberOfPaletteColors;
+  console.log(paletteColors.length);
 
   $(".canvas").css({
     padding: padding,
@@ -33,11 +37,15 @@ $(document).ready(function () {
   $('.pixel[tabIndex=1]').focus();
  
   if(availableColors.length>0){
+    $(".wrapper").append('<div class="buttomPicker"></div>');
     for(let i=0;i<availableColors.length;i++){
       $(".bottomPicker").append(`<div class="color_bottom" id="color${i+1}" tabIndex="${i+1}" style="background-color:${availableColors[i]};"></div>`);
     }
     $(".bottomPicker").append(`<div class="space"></div>`);
+  }else{
+    $(".wrapper").append('<div tabIndex="1" class="customPicker"></div>');
   }
+
 
   $(".color_bottom").each(function(){
     $(this).keydown(function(){
@@ -47,6 +55,25 @@ $(document).ready(function () {
     });
   });
 
+  $(".customPicker").keydown(function(){
+    $("#colorModal").modal({
+      showClose:false,
+    });
+  });
+
+  $(".modalColor").css({
+    padding: padding,
+    width:canvasSize,
+    height:canvasSize,
+    "grid-template-columns": `repeat(${numberOfPaletteColors},${sizeOfColor}px)`,
+    "grid-template-rows": `repeat(${numberOfPaletteColors},${sizeOfColor}px)`,
+
+  });
+
+  for (let i = 0; i < Math.pow(paletteColors.length,2); i++) {
+    $(".modalColor").append(`<div class="colorPixel" id="colorPixel${i+1}" tabIndex="${i+1}" style="background-color:${paletteColors[i]};"></div>`);    
+  }
+
   const softkeycallbackGridColor= {
     center: function() { 
       let focused=$(":focus");
@@ -54,20 +81,24 @@ $(document).ready(function () {
         availableColors.length>0?$(":focus").css({"background-color":localStorage.getItem("currentColor")}):$(":focus").css({"background-color":null});
       } 
      },
-     left:function(){  
-       if(availableColors.length>0){
-        isGrid=!isGrid;
-        var fGrid= document.getElementById(localStorage.getItem("fGrid"));
-        var fColor=document.getElementById(localStorage.getItem("fColor"));
-        $(":focus").blur();
+     left:function(){ 
+      isGrid=!isGrid;
+      var fGrid= document.getElementById(localStorage.getItem("fGrid")); 
+      $(":focus").blur();
+       if(availableColors.length>0){ 
+        var fColor=document.getElementById(localStorage.getItem("fColor")); 
         if(isGrid){
          fGrid?fGrid.focus():$("#pixel1").focus();
         }else{
           fColor?fColor.focus():$("#color1").focus();
         } 
        } else{
-        window.location.href = "./displayColor.html";
-       }  
+        if(isGrid){
+          fGrid?fGrid.focus():$("#pixel1").focus();
+         }else{
+          $(".customPicker").focus();
+         }
+       }
      }
   };
   document.addEventListener('keydown',handleKeyDownGrid);
