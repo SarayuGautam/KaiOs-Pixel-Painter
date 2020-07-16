fac91f14f902c3bbe13f1c2370da3c4743008633
Sarayu Gautam <sarayugautam1@gmail.com>
2020-07-16 19:58:16 +0545

    final for today

--- js/gridFunc.js
+++ js/gridFunc.js
@@ -10,8 +10,8 @@ $(document).ready(function () {
   const coloringNumber = localStorage.getItem("coloringNumber") ? JSON.parse(localStorage.getItem("coloringNumber")) : [];
   const numberColorAssociation = JSON.parse(localStorage.getItem("numberColorAssociation"));
   const colorGrid = JSON.parse(localStorage.getItem('colorGrid'));
-  // const availableColors = JSON.parse(localStorage.getItem('availableColors')) || [];
-  const availableColors =[];
+  const availableColors = JSON.parse(localStorage.getItem('availableColors')) || [];
+  // const availableColors =[];
   const numberOfColors = availableColors.length;
   var isGrid = true;
   const paletteColors = ["#003366", "#336699", "#3366CC", "#003399", "#000099", "#0000CC", "#000066", "#006666", "#006699", "#0099CC", "#0066CC", "#0033CC", "#0000FF", "#3333FF", "#333399", "#669999", "#009999", "#33CCCC", "#00CCFF", "#0099FF", "#0066FF", "#3366FF", "#3333CC", "#666699", "#339966", "#00CC99", "#00FFCC", "#00FFFF", "#33CCFF", "#3399FF", "#6699FF", "#6666FF", "#6600FF", "#6600CC", "#339933", "#00CC66", "#00FF99", "#66FFFF", "#99CCFF", "#9999FF", "#9966FF", "#9933FF", "#9900FF", "#006600", "#00CC00", "#00FF00", "#66FF99", "#99FFCC", "#CCCCFF", "#CC99FF", "#CC66FF", "#CC33FF", "#CC00FF", "#9900CC", "#003300", "#009933", "#33CC33", "#66FF66", "#99FF99", "#CCFFCC", "#FF99FF", "#FF66FF", "#FF00FF", "#CC00CC", "#660066", "#336600", "#009900", "#66FF33", "#99FF66", "#CCFF99", "#FFFFCC", "#FFCCCC", "#FF99CC", "#FF33CC", "#CC0099", "#993399", "#333300", "#669900", "#99FF33", "#CCFF66", "#FFFF99", "#FFCC99", "#FF9999", "#FF6699", "#FF3399", "#CC3399", "#990099", "#666633", "#99CC00", "#CCFF33", "#FFFF66", "#FFCC66", "#FF9966", "#FF6666", "#FF0066", "#CC6699", "#993366", "#999966", "#CCCC00", "#FFFF00", "#FFCC00", "#FF9933", "#FF6600", "#FF5050", "#CC0066", "#660033", "#996633", "#CC9900", "#FF9900", "#CC6600", "#FF3300", "#FF0000", "#CC0000", "#990033", "#663300", "#996600", "#CC3300", "#993300", "#990000", "#800000", "#993333"];
@@ -82,7 +82,10 @@ if(buttonIndex==1){
 
 
 
-
+ if(document.activeElement.hasClass('color_bottom')||document.activeElement.hasClass('customPicker')){
+   $('#softkey-left').text('drawing');
+   $('#softkey-center').text('SELECT');
+ }
 
 
 
@@ -183,7 +186,7 @@ if(buttonIndex==1){
       isGrid = !isGrid;
       var fGrid = document.getElementById(localStorage.getItem("fGrid"));
       $(":focus").blur();
-      if (availableColors.length > 0) {
+      if (availableColors.length > 0||buttonIndex==1) {
         var fColor = document.getElementById(localStorage.getItem("fColor"));
         if (isGrid) {
           fGrid ? fGrid.focus() : $("#pixel1").focus();
